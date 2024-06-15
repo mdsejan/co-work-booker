e534b721a9bf5a080c5cfee1bba62ec2628b03b1
Sejan~ <md4sejan@gmail.com>
2024-06-15 22:03:06 +0600

    password hash using bcrypt

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, model } from "mongoose";
 import { IUser } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 const UserSchema = new Schema<IUser>({
   name: { type: String, required: true },
@@ -9,6 +11,13 @@ const UserSchema = new Schema<IUser>({
   address: { type: String, required: true },
   role: { type: String, required: true, enum: ["user", "admin"] },
   isDeleted: { type: Boolean, default: false },
+});
+
+UserSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcrypt_salt_rounds)
+  );
 });
 
 export const UserModel = model<IUser>("User", UserSchema);
